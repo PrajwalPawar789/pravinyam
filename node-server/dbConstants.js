3293831ed2ecc15f997643e41ff394f8c3d30216
PrajwalPawar789 <114561900+PrajwalPawar789@users.noreply.github.com>
2022-09-30 10:17:00 +0530

    Delete node-server directory

--- node-server/dbConstants.js
+++
@@ -1,6 +0,0 @@
-// This file is used to define schema names, table names, blob storage(spaces location) etc.
-
-export const PRAVINYAM_SCHEMA = 'Public'; // use real schema name here
-export const TABLE_EXERCISES = 'cexercises';
-export const TABLE_QA = 'cqadata';
-export const SPACES_CODE_FILES = 'pravinyam-programs'
--- node-server/test.js
+++
@@ -1,3 +0,0 @@
-var currentdate = new Date(); 
-var datetime =  currentdate.getFullYear() + "-"+ (currentdate.getMonth()+1)  + "-" + currentdate.getDate() + " "  + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":" + currentdate.getSeconds() + "." + currentdate.getMilliseconds();
-console.log(datetime);
